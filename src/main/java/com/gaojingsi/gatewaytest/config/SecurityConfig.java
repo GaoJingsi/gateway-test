261e8da81ef7ea4f7594b46a4a0923ec1407f9d2
GaoJingsi <1040147371@qq.com>
2020-06-06 16:38:25 +0800

    加入spring security,未完

---
+++ src/main/java/com/gaojingsi/gatewaytest/config/SecurityConfig.java
@@ -0,0 +1,94 @@
+package com.gaojingsi.gatewaytest.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrations;
+import org.springframework.security.oauth2.client.registration.InMemoryReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.filter.reactive.HiddenHttpMethodFilter;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilterChain;
+import org.springframework.web.server.session.CookieWebSessionIdResolver;
+import org.springframework.web.server.session.WebSessionIdResolver;
+import reactor.core.publisher.Mono;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+//    @Bean
+//    public MapReactiveUserDetailsService userDetailsService() {
+//        UserDetails user = User.withDefaultPasswordEncoder()
+//                .username("user")
+//                .password("user")
+//                .roles("USER")
+//                .build();
+//        return new MapReactiveUserDetailsService(user);
+//    }
+
+//    @Bean
+//    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+//        http
+//                // ...
+//                .oauth2Login(withDefaults());
+//        return http.build();
+//    }
+
+    @Bean
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http, ReactiveClientRegistrationRepository clientRegistrations) {
+        http
+                // ...
+                .authorizeExchange(exchange -> {
+                    exchange.anyExchange().authenticated();
+                })
+                .oauth2Login(oauth2 -> oauth2
+                        .clientRegistrationRepository(clientRegistrations)
+                )
+                .csrf(ServerHttpSecurity.CsrfSpec::disable);
+        return http.build();
+    }
+
+//    @Bean
+//    ReactiveClientRegistrationRepository clientRegistrations() {
+//        ClientRegistration clientRegistration = ClientRegistrations
+//                .fromIssuerLocation("http://192.168.0.254:8000/.well-known/oauth-authorization-server/issuer")
+//                .clientId("yibaoAppSecret")
+//                .clientSecret("qaManagerSecret")
+//                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+//                .scope("all")
+//                .tokenUri("http://192.168.0.254:8000/oauth/token")
+//                .build();
+//        return new InMemoryReactiveClientRegistrationRepository(clientRegistration);
+//    }
+
+    @Bean
+    public WebSessionIdResolver webSessionIdResolver() {
+        CookieWebSessionIdResolver resolver = new CookieWebSessionIdResolver();
+        // 重写定义 cookie 名字
+        resolver.setCookieName("GATEWAYSESSIONID");
+        resolver.addCookieInitializer((builder) -> builder.path("/"));
+        resolver.addCookieInitializer((builder) -> builder.sameSite("Strict"));
+        return resolver;
+    }
+
+    @Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+        return new HiddenHttpMethodFilter() {
+            @Override
+            public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+                return chain.filter(exchange);
+            }
+        };
+    }
+
+}
